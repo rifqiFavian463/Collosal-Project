4a2f53c5efc1f672ade7392b772c4ccbd3d0d8bf
Rifqi Favian <piyanh90@gmail.com>
2023-05-21 11:35:04 +0700

    testing utils

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
+      <button className="button-tests">Detail</button>
     </>
   );
 }
