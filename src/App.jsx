bd24edb7da4487ab850173d858de8504899d0a9d
Rifqi Favian <piyanh90@gmail.com>
2023-05-21 12:21:47 +0700

    test 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ function App() {
       <p className="collosal-tagline">TAGLINEE!</p>
       <h1 className="collosal-title">Hello world!</h1>
       <p className="collosal-description">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque similique nostrum delectus, ipsum culpa cupiditate nemo a consectetur dolores harum cum voluptate quaerat libero necessitatibus
-        iusto aperiamg
+        little bit change. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam corporis commodi labore magni fuga assumenda, vitae est velit ipsum exercitationem!
       </p>
     </>
   );
