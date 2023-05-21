16ac9b2bd83b52ad70197828cc42b94c0a67df5c
Rifqi Favian <piyanh90@gmail.com>
2023-05-21 11:43:30 +0700

    test branch

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
       <h1 className="collosal-title">Hello world!</h1>
       <p className="collosal-description">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque similique nostrum delectus, ipsum culpa cupiditate nemo a consectetur dolores harum cum voluptate quaerat libero necessitatibus
-        iusto aperiam ducimus quidem consequatur!
+        iusto aperiamg
       </p>
     </>
   );
